59dc3401d95f1343774425de710df693e45366fe
Admiral Potato <admiral@nuclearpixel.com>
2018-09-03 16:27:23 -0600

    Replace mesh primitives with Snatom style molecules loaded from PubChem

--- 2d_overlay_3d.js
+++ 2d_overlay_3d.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('3d')
 const svg = document.getElementById('2d')
 const xmlns = svg.getAttribute('xmlns')
-const xlinkns = "http://www.w3.org/1999/xlink";
+const xlinkns = 'http://www.w3.org/1999/xlink';
 const renderer = new THREE.WebGLRenderer({
   canvas,
   alpha: true,
@@ -32,6 +32,7 @@ scene.add(ambientLight)
 
 const light = new THREE.HemisphereLight( 0xcefeff, 0xb3eaf0, .5 )
 scene.add( light )
+scene.fog = new THREE.Fog('#262626', 7, 9)
 
 camera.position.set(0, 0, 8)
 camera.lookAt(new THREE.Vector3(0,0,0))
@@ -83,7 +84,8 @@ const loop = (time) => {
     animate(time)
   }
 }
-const start = () => {
+const start = async () => {
+  await makeObjects()
   requestAnimationFrame(loop)
 }
 
@@ -92,9 +94,10 @@ const getScreenXY = (object) => {
   vector.setFromMatrixPosition(object.matrixWorld)
   vector.project(camera);
 
-  return new THREE.Vector2(
-    (1 - (( vector.x + 1 ) * 0.5)) * width,
-    ((( vector.y + 1 ) * 0.5)) * height
+  return new THREE.Vector3(
+    ((( vector.x + 1 ) * 0.5)) * width,
+    (1 - (( vector.y + 1 ) * 0.5)) * height,
+    (( vector.z + 1 ) * 0.5) * square
   )
 }
 
@@ -104,8 +107,6 @@ const defs = document.createElementNS(xmlns, 'defs')
 const overlayDef = document.createElementNS(xmlns, 'g')
 const circle = document.createElementNS(xmlns, 'circle')
 const line = document.createElementNS(xmlns, 'path')
-const miscLines = document.createElementNS(xmlns, 'path')
-miscLines.setAttributeNS(null, 'class', 'stroke')
 overlayDef.setAttributeNS(null, 'id', 'overlay')
 circle.setAttributeNS(null, 'class', 'stroke')
 line.setAttributeNS(null, 'class', 'stroke')
@@ -115,70 +116,125 @@ defs.appendChild(overlayDef)
 svg.appendChild(defs)
 
 let circleRadius
+let fontSize
 const reziseDefs = () => {
-  const strokeWidth = '' + (square / 150)
-  circleRadius = square / 16
+  const strokeWidth = '' + (square / 300)
+  circleRadius = square / 15
+  fontSize = (circleRadius * 0.35) + 'px'
   const lsa = circleRadius * 0.575
   const lsb = circleRadius * 0.525
   circle.setAttributeNS(null, 'r', circleRadius)
-  circle.setAttributeNS(null, 'stroke-width', strokeWidth)
-  line.setAttributeNS(null, 'stroke-width', strokeWidth)
   line.setAttributeNS(null, 'd', `
     M-${lsa},-${lsa} L-${lsb},-${lsb}Z
     M${lsa},-${lsa} L${lsb},-${lsb}Z
     M-${lsa},${lsa} L-${lsb},${lsb}Z
     M${lsa},${lsa} L${lsb},${lsb}Z
   `)
-  miscLines.setAttributeNS(null, 'stroke-width', strokeWidth)
-}
-
-const geometry = new THREE.DodecahedronGeometry(0.25, 0)
-const palette = ["#D4E6E0", "#EE720A", "#557E86", "#9E650F", "#475B4F", "#D8B85B"]
-const mats = [
-  new THREE.MeshStandardMaterial({color: palette[0], metalness: 0.1, roughness: 0.5}),
-  new THREE.MeshStandardMaterial({color: palette[1], metalness: 0.1, roughness: 0.5}),
-  new THREE.MeshStandardMaterial({color: palette[2], metalness: 0.1, roughness: 0.5}),
-  new THREE.MeshStandardMaterial({color: palette[3], metalness: 0.1, roughness: 0.5}),
-  new THREE.MeshStandardMaterial({color: palette[4], metalness: 0.1, roughness: 0.5}),
-  new THREE.MeshStandardMaterial({color: palette[5], metalness: 0.1, roughness: 0.5}),
-]
-
-const completeSets = 3
-let objects = []
-svg.appendChild(miscLines)
-for (let i = 0; i < mats.length * completeSets; i++) {
-  const pivotA = new THREE.Group()
-  const pivotB = new THREE.Group()
-  const shape = new THREE.Mesh(geometry, mats[i % mats.length])
-  const overlay = document.createElementNS(xmlns, 'g')
-  const use = document.createElementNS(xmlns, 'use')
-  const text = document.createElementNS(xmlns, 'text')
-  pivotA.position.x = 1.75
-  shape.position.x = 0.375
-  shape.castShadow = shape.receiveShadow = true
-  pivotA.add(shape)
-  pivotB.add(pivotA)
-  group.add(pivotB)
-  use.setAttributeNS(xlinkns, 'xlink:href', '#overlay')
-  text.setAttributeNS(null, 'class', 'text')
-  text.setAttributeNS(null, 'text-anchor', 'middle')
-  text.setAttributeNS(null, 'alignment-baseline', 'central')
-  text.textContent = i % mats.length
-  overlay.appendChild(use)
-  overlay.appendChild(text)
-  svg.appendChild(overlay)
-
-  objects.push({
-    pivotA,
-    pivotB,
-    shape,
-    use,
-    text,
-    overlay
+  document.querySelectorAll('.stroke').forEach((element) => {
+    element.setAttributeNS(null, 'stroke-width', strokeWidth)
   })
 }
 
-const loopDuration = 4
+const materialBase = {metalness: 0.1, roughness: 0.5}
+const atomDataMap = {
+  h: {scale: 0.5, material: new THREE.MeshStandardMaterial({color: '#b1cbbf', ...materialBase})},
+  c: {scale: 1.0, material: new THREE.MeshStandardMaterial({color: '#425d61', ...materialBase})},
+  o: {scale: 1.1, material: new THREE.MeshStandardMaterial({color: '#c41715', ...materialBase})},
+  n: {scale: 1.1, material: new THREE.MeshStandardMaterial({color: '#0b5366', ...materialBase})},
+  cl: {scale: 1.2, material: new THREE.MeshStandardMaterial({color: '#216359', ...materialBase})},
+  na: {scale: 1.2, material: new THREE.MeshStandardMaterial({color: '#474a9c', ...materialBase})},
+}
+const molecules = [
+  {name: 'Sucrose', path: 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/5988/sdf?record_type=3d'},
+  {name: 'Nicotine', path: 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/89594/sdf?record_type=3d'},
+  {name: 'Cholesterol', path: 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/5997/sdf?record_type=3d', rotation: new THREE.Euler(0, 0, -Math.PI / 2)},
+  {name: 'Caffeine', path: 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/2519/sdf?record_type=3d'},
+  {name: 'Salt', path: 'https://cactus.nci.nih.gov/chemical/structure/[Cl-][Na+]/file?format=sdf&get3d=True'},
+  {name: 'Alcohol', path: 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/5798/sdf?record_type=3d'},
+  {name: 'THC', path: 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/16078/sdf?record_type=3d'},
+  {name: 'Methylphenidate', path: 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/4158/sdf?record_type=3d'}
+]
+
+let objects = []
+const completeSets = 2
+const makeRequest = async (url) => {
+  return fetch(
+    url,
+    {
+      method: 'GET',
+      cache: 'force-cache'
+    }
+  )
+}
+const makeObjects = async () => {
+  const parsedMolecules = await Promise.all(molecules.map(async (molecule) => {
+    let response
+    while (
+      !response ||
+      (
+        !response.ok &&
+        response.status >= 500
+      )) {
+      // sometimes nih.gov returns 500 range errors - try again
+      if (response) {
+        console.log(`Error loading "${molecule.path}", trying again`)
+      }
+      response = await makeRequest(molecule.path)
+    }
+    molecule.sdfText = response.ok ? await response.text() : ''
+    const parsedMolecule = sdfTextToThreeSnatom(molecule.sdfText, atomDataMap, molecule.rotation)
+    molecule.data = parsedMolecule.data
+    molecule.model = parsedMolecule.model
+    return molecule
+  }))
+  let moleculeScale = -Infinity
+  let moleculeScaleVector = new THREE.Vector3()
+  molecules.forEach((molecule) => {
+    molecule.data.bounds.getSize(moleculeScaleVector)
+    moleculeScale = Math.max(moleculeScale, moleculeScaleVector.length())
+    molecule.model.children[0].position.sub(
+      molecule.data.bounds.getCenter(moleculeScaleVector)
+    )
+  })
+  for (let i = 0; i < molecules.length * completeSets; i++) {
+    const moleculeIndex = i % molecules.length
+    const pivotA = new THREE.Group()
+    const pivotB = new THREE.Group()
+    const molecule = molecules[moleculeIndex].model.clone(true)
+    const overlay = document.createElementNS(xmlns, 'g')
+    const use = document.createElementNS(xmlns, 'use')
+    const text = document.createElementNS(xmlns, 'text')
+    const line = document.createElementNS(xmlns, 'path')
+    pivotA.position.x = 1.25
+    molecule.scale.multiplyScalar(1 / moleculeScale)
+    molecule.position.x = 0.75
+    pivotA.add(molecule)
+    pivotB.add(pivotA)
+    group.add(pivotB)
+    use.setAttributeNS(xlinkns, 'xlink:href', '#overlay')
+    text.setAttributeNS(null, 'class', 'text')
+    text.setAttributeNS(null, 'text-anchor', 'middle')
+    text.setAttributeNS(null, 'alignment-baseline', 'central')
+    line.setAttributeNS(null, 'class', 'stroke')
+    text.textContent = molecules[moleculeIndex].name
+    overlay.appendChild(use)
+    overlay.appendChild(text)
+    overlay.appendChild(line)
+    svg.appendChild(overlay)
+
+    objects.push({
+      pivotA,
+      pivotB,
+      molecule,
+      line,
+      use,
+      text,
+      overlay
+    })
+  }
+}
+
+const loopDuration = 20
 const tau = Math.PI * 2
 const diff = new THREE.Vector2()
 const pointA = new THREE.Vector2()
@@ -186,7 +242,8 @@ const pointB = new THREE.Vector2()
 const pointC = new THREE.Vector2()
 const rotatedRadius = new THREE.Vector2()
 const circleRadiusVector = new THREE.Vector2()
-const emptyVector = new THREE.Vector2(0, 0,)
+const emptyVector = new THREE.Vector2(0, 0)
+const clamp = (n) => { return Math.min(Math.max(0, n), 1) }
 const animate = (time) => {
   const phase = time / 1000 / loopDuration / completeSets
   const objectFrac = 1 / objects.length
@@ -194,31 +251,29 @@ const animate = (time) => {
   objects.forEach((object, index) => {
     const frac = index * objectFrac
     object.pivotB.rotation.z = (phase + frac) * tau
-    object.pivotA.rotation.y = (phase + frac) * tau * 6
-    object.shape.rotation.y = (phase + frac) * tau * 4
+    object.pivotA.rotation.y = (phase + frac) * tau * 4
+    object.molecule.rotation.z = (phase + frac) * tau * -2
   })
   renderer.render(scene, camera)
   // 2D loop needs to be run after the 3D render so all matrices are "baked"
   const lineInstructions = []
   circleRadiusVector.set(circleRadius, 0)
   objects.forEach((object) => {
-    const position = getScreenXY(object.shape)
+    const position = getScreenXY(object.molecule)
     diff.copy(position).sub(center)
     const angle = diff.angle()
     rotatedRadius.copy(circleRadiusVector).rotateAround(emptyVector, angle)
-    pointA.copy(rotatedRadius).multiplyScalar(1.250).add(diff).add(center)
-    pointB.copy(rotatedRadius).multiplyScalar(1.500).add(diff).add(center)
+    pointA.copy(rotatedRadius).multiplyScalar(1.000)
+    pointB.copy(rotatedRadius).multiplyScalar(1.500)
     pointC.copy(rotatedRadius).multiplyScalar(2.000)
-    lineInstructions.push(
-      `M${pointA.x},${pointA.y} L${pointB.x},${pointB.y}Z`,
-    )
+    object.line.setAttributeNS(null, 'd', `M${pointA.x},${pointA.y} L${pointB.x},${pointB.y}Z`)
     object.use.setAttributeNS(null, 'transform', `rotate(${-(angle / deg) * 2})`)
+    object.overlay.setAttributeNS(null, 'opacity', `${1 - clamp(((position.z / square) - 0.9863) * 1000) }`)
     object.overlay.setAttributeNS(null, 'transform', `translate(${position.x}, ${position.y})`)
     object.text.setAttributeNS(null, 'x', pointC.x)
     object.text.setAttributeNS(null, 'y', pointC.y)
-    object.text.style.setProperty('font-size', (circleRadius * 0.75) + 'px')
+    object.text.style.setProperty('font-size', fontSize)
   })
-  miscLines.setAttributeNS(null, 'd', lineInstructions.join(' '))
 }
 
 start()
---
+++ sdf_to_three.js
@@ -0,0 +1,79 @@
+const atomGeometry = new THREE.IcosahedronGeometry(1, 3)
+const instructionHandlers = {
+  // atom
+  '16': (args, data) => {
+    if(args[0] !== 'M'){
+      data.atoms.push({
+        vector: new THREE.Vector3(
+          parseFloat(args[0]),
+          parseFloat(args[1]),
+          parseFloat(args[2])
+        ),
+        symbol: args[3]
+      })
+    }
+  },
+  // bond
+  '7': (args, data) => {
+    if(args[0] !== 'M'){
+      data.bonds.push({
+        a: parseInt(args[0]) - 1,
+        b: parseInt(args[1]) - 1,
+        bonds: parseInt(args[2])
+      })
+    }
+  }
+}
+
+const sdfTextToThreeSnatom = (sdfText, atomDataMap, rotation) => {
+  let data = {
+    atoms: [],
+    bonds: [],
+    maxBondLength: -Infinity,
+    minBondLength: Infinity,
+    bounds: new THREE.Box3()
+  }
+  let model = new THREE.Group()
+  let molecule = new THREE.Group()
+  const instructions = sdfText.split('M  END').shift().split('\n')
+  instructions.forEach((instructionLine) => {
+    const args = instructionLine
+      .replace(/[ \t]+/g, ' ')
+      .replace(/^ /, '')
+      .split(' ')
+    const instrunctionHandler = instructionHandlers[args.length.toString()]
+    if (instrunctionHandler) {
+      instrunctionHandler(args, data)
+    }
+  })
+  data.bonds.forEach((bond) => {
+    const atomA = data.atoms[bond.a]
+    const atomB = data.atoms[bond.b]
+    bond.length = atomA.vector.distanceTo(atomB.vector)
+    data.maxBondLength = Math.max(data.maxBondLength, bond.length)
+    data.minBondLength = Math.min(data.minBondLength, bond.length)
+  })
+  const baseAtomScale = data.maxBondLength * 0.5
+  data.atoms.forEach((atom) => {
+    const atomData = atomDataMap[atom.symbol.toLocaleLowerCase()]
+    if(!atomData) {
+      console.error('Could not find material for: ', atom.symbol)
+    }
+    let ball = new THREE.Mesh(atomGeometry, atomData.material)
+    ball.position.add(atom.vector)
+    ball.castShadow = ball.receiveShadow = true
+    ball.scale.multiplyScalar(baseAtomScale * atomData.scale)
+    molecule.add(ball)
+  })
+  data.bounds.expandByObject(molecule)
+  molecule.scale.multiplyScalar(1 / baseAtomScale)
+  if (rotation) {
+    molecule.rotation.copy(rotation)
+  }
+  // console.log(data)
+  model.add(molecule)
+  return {
+    data,
+    model
+  }
+}
